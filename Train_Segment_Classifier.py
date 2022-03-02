5aa878066d9978959c0bc4f73e873873c58721c5
Hossein Biniazian <36810983+iamhosseinbiniazian@users.noreply.github.com>
2022-03-02 17:12:26 +0330

    Update Train_Segment_Classifier.py

--- Train_Segment_Classifier.py
+++ Train_Segment_Classifier.py
@@ -4,19 +4,19 @@ import numpy as np
 import os
 folder='data/genresTrainTest/test/'
 #directory for genere music classification
-dirname=['/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/blues',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/classical',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/country',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/disco',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/hiphop',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/jazz',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/metal',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/pop',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/reggae',
-         '/home/quadro2/PycharmProjects/PyTest/data/genresTrainTest/train/rock']
+dirname=['data/genresTrainTest/train/blues',
+         'data/genresTrainTest/train/classical',
+         'data/genresTrainTest/train/country',
+         'data/genresTrainTest/train/disco',
+         'data/genresTrainTest/train/hiphop',
+         'data/genresTrainTest/train/jazz',
+         'data/genresTrainTest/train/metal',
+         'data/genresTrainTest/train/pop',
+         'data/genresTrainTest/train/reggae',
+         'data/genresTrainTest/train/rock']
 #direcxtory for speech/music classification
-dirnamesm=['/home/quadro2/PycharmProjects/PyTest/data/MusicSpeech/music',
-           '/home/quadro2/PycharmProjects/PyTest/data/MusicSpeech/speech']
+dirnamesm=['data/MusicSpeech/music',
+           'data/MusicSpeech/speech']
 #aT.featureAndTrain(dirnamesm, 1.0, 1.0, aT.shortTermWindow,aT.shortTermStep, "svm", "svmMusicSpeech", computeBEAT=False)
 
 #[flagsInd, classesAll, acc, CM] = aS.mtFileClassification("data/scottish.wav", "svmMusicSpeech", "svm", True, 'data/scottish.segments')
