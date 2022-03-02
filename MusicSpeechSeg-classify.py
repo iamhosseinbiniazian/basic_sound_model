571e14e4961d0075c082c0bf677f4548af18858a
Hossein Biniazian <36810983+iamhosseinbiniazian@users.noreply.github.com>
2022-03-02 17:06:40 +0330

    Update MusicSpeechSeg-classify.py

--- MusicSpeechSeg-classify.py
+++ MusicSpeechSeg-classify.py
@@ -1,8 +1,8 @@
 from PyAudio import audioTrainTest as aT
 from PyAudio import audioSegmentation as aS
 
-dirnamesm=['/home/quadro2/PycharmProjects/PyTest/data/MusicSpeech/music',
-           '/home/quadro2/PycharmProjects/PyTest/data/MusicSpeech/speech']
+dirnamesm=['data/MusicSpeech/music',
+           'data/MusicSpeech/speech']
 #aT.featureAndTrain(dirnamesm, 1.0, 1.0, aT.shortTermWindow,aT.shortTermStep, "svm", "svmMusicSpeech", computeBEAT=False)
 
 [flagsInd, classesAll, acc, CM] = aS.mtFileClassification("out2.WAV", "svmModel", "svm", True)
