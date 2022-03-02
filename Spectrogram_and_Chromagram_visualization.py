52ad0c0ce29104a85c32806ece87f6b90645101f
Hossein Biniazian <36810983+iamhosseinbiniazian@users.noreply.github.com>
2022-03-02 17:10:12 +0330

    Update Spectrogram_and_Chromagram_visualization.py

--- Spectrogram_and_Chromagram_visualization.py
+++ Spectrogram_and_Chromagram_visualization.py
@@ -1,5 +1,5 @@
 from PyAudio import audioAnalysis
-wavefilename='/media/hossein/New Volume/Source code/PycharmProjects/PyTest/data/doremi.wav'
+wavefilename='data/doremi.wav'
 #Spectrogram
 #audioAnalysis.fileSpectrogramWrapper(wavefilename)
 #Chromagram
