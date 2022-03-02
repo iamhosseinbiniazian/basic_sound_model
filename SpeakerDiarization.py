3d4f867b511332c4968a308218c0b4f01eaf91a6
Hossein Biniazian <36810983+iamhosseinbiniazian@users.noreply.github.com>
2022-03-02 17:09:48 +0330

    Update SpeakerDiarization.py

--- SpeakerDiarization.py
+++ SpeakerDiarization.py
@@ -1,6 +1,6 @@
 from PyAudio import audioBasicIO as aIO
 from PyAudio import audioSegmentation as aS
-soundpathth='/media/hossein/New Volume/Source code/PycharmProjects/PyTest/data/diarizationExample.wav'
+soundpathth='data/diarizationExample.wav'
 def mainSpeakerDiarization(soundpath,numspeaker):
     m=aS.speakerDiarization(soundpath,
                         numOfSpeakers=numspeaker)
