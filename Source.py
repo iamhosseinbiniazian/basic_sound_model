cb8be6237800cb424536bca5322cfc4f4a11f00b
Hossein Biniazian <36810983+iamhosseinbiniazian@users.noreply.github.com>
2022-03-02 17:08:34 +0330

    Update Source.py

--- Source.py
+++ Source.py
@@ -4,8 +4,8 @@ from PyAudio import audioTrainTest as aT
 import os
 from sys import argv
 from PyAudio import convertToWav
-subdirectories = ["/home/quadro2/PycharmProjects/audioClassification/trainingData/bomb",\
-                  "/home/quadro2/PycharmProjects/audioClassification/trainingData/cry",\
-                  "/home/quadro2/PycharmProjects/audioClassification/trainingData/gun",\
-                  "/home/quadro2/PycharmProjects/audioClassification/trainingData/laugh"]
+subdirectories = ["data/audioClassification/trainingData/bomb",\
+                  "data/audioClassification/trainingData/cry",\
+                  "data/audioClassification/trainingData/gun",\
+                  "data/audioClassification/trainingData/laugh"]
 aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmModel2", False)
